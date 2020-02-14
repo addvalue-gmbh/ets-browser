2df294563b412cdb846dd9181d0795b10895f328
Tom Meiselbach <tom.meiselbach@addvalue.de>
2020-02-14 11:01:09 +0100

    Added typespecs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EtsBrowser.MixProject do
   def project do
     [
       app: :ets_browser,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
